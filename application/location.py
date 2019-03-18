cdfbce53e71e2b78be830fa49b17858f616c2783
matasollalucia <matasollalucia@gmail.com>
2019-03-18 19:45:54 +0100

    Ammended last commit

--- application/location.py
+++ application/location.py
@@ -8,8 +8,9 @@ import requests
 def handle_location(message):
 	latitude = message.location.latitude
 	longitude = message.location.longitude
-	nombre, imagen = patiocercano(latitude, longitude)
-	bot.reply_to(message, nombre)
+	nombre, imagen = patio_cercano(latitude, longitude)
+	bot.reply_to(message, "El patio más cercano está en " + nombre)
+	#bot.reply_to(message, desc)
 	bot.reply_to(message, imagen)
 
 def distance(lat1, long1, lat2, long2):
@@ -17,7 +18,7 @@ def distance(lat1, long1, lat2, long2):
 	print("dist:" + str(dist))
 	return dist
 
-def patiocercano(mylatitude, mylongitude):
+def patio_cercano(mylatitude, mylongitude):
 	url = "http://b.patios.cordoba.es/api/v1/public/{locale}/json/pois_y_zonas"
 
 	headers = {
@@ -29,13 +30,15 @@ def patiocercano(mylatitude, mylongitude):
 	parsed = json.loads(data)
 
 	distanciaMinima = 999
+	size = len(parsed['pois']) - 1
 
-	for x in range(0, 72):
+	for x in range(0, size):
 		latitude = float(parsed['pois'][x]['lat'])
 		longitude = float(parsed['pois'][x]['lng'])
 
 		if distance(latitude, longitude, mylatitude, mylongitude) < distanciaMinima:
 			nombre = parsed['pois'][x]['nombre']
+			#desc = parsed['pois'][x]['descripcion']
 			imagen = parsed['pois'][x]['imagen']
 			distanciaMinima = distance(latitude, longitude, mylatitude, mylongitude)
 
