1e0813cddf522e79865da41490a666ef6d33c74f
Sergio Gómez <sergio@uco.es>
2019-03-19 20:56:52 +0100

    No more echo

--- application/commands.py
+++ application/commands.py
@@ -7,13 +7,3 @@ longitude = float('nan')
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
-
-
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_message(message):
-    """
-    Hace un 'eco' de lo que se recibe y no se ha procesado en algún comando anterior.
-    :param message:
-    :return:
-    """
-    bot.reply_to(message, message.text)
